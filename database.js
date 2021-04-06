2e4f31c79f737cd9209e21e65896787035ee478f
Final Bosss <68625695+tony-dark-tony@users.noreply.github.com>
2021-04-07 06:03:20 +0700

    Update database.js

--- database.js
+++ database.js
@@ -13,6 +13,7 @@ const sequelize = new Sequelize('thaibui', 'administrators', 'Root@123', {
     }
   });
 sequelize.authenticate()
+sequelize.sync()
 .then(()=>{ console.log("Success !")})
 .catch((err)=>{ console.log(err)})
 const videos = sequelize.define('videos', {
@@ -28,56 +29,13 @@ const insta_acccount = sequelize.define('insta_account', {
   password: Sequelize.TEXT,
   status: Sequelize.TEXT,
 })
-videos.bulkCreate([
-  {
-    drive_url: "https://drive.google.com/file/d/11FwWeLuesj3IgkkocGIMZe4GxXLpL2Sh/view?usp=sharing",
-    title: "Test 1 video",
-    description: "This is a test video 1",
-    insta_id: "1",
-    insta_url: "instagram.com",
-    status: "Completed"
-  },
-  {
-    drive_url: "https://drive.google.com/file/d/11FwWeLuesj3IgkkocGIMZe4GxXLpL2Sh/view?usp=sharing",
-    title: "Test 2 video",
-    description: "This is a test video 2",
-    insta_id: "2",
-    insta_url: "instagram.com",
-    status: "Completed"
-  },
-  {
-    drive_url: "https://drive.google.com/file/d/11FwWeLuesj3IgkkocGIMZe4GxXLpL2Sh/view?usp=sharing",
-    title: "Test 3 video",
-    description: "This is a test video 3",
-    insta_id: "3",
-    insta_url: "instagram.com",
-    status: "Incompleted"
-  },
-  {
-    drive_url: "https://drive.google.com/file/d/11FwWeLuesj3IgkkocGIMZe4GxXLpL2Sh/view?usp=sharing",
-    title: "Test 4 video",
-    description: "This is a test video 4",
-    insta_id: "4",
-    insta_url: "instagram.com",
-    status: "Completed"
-  },
-])
-insta_acccount.bulkCreate([
-  {
-    username: "tonytony",
-    password: "parkerparker",
-    status: "LIVE"
-  },
-  {
-    username: "testing1",
-    password: "phamminh",
-    status: "DEATH"
-  },
-  {
-    username: "testing2",
-    password: "helloworld",
-    status: "LIVE"
-  },
-])
+getData(data=>{
+  console.log(data)
+})
 sequelize.sync()
 
+function getData(callback){
+  videos.findAll({raw:true}).then(task=>{
+    callback(task)
+  })
+}
